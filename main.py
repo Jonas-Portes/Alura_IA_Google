ebe47e2fb8e94d866207f0e7b709852551c18198
Jonas Portes <jonas.portes@hotmail.com>
2025-09-17 19:27:46 -0300

    First step towards Dva

--- main.py
+++ main.py
@@ -59,16 +59,6 @@ def triagem(mensagem: str) -> Dict:
     return saida.model_dump()
 
 
-testes = ["Posso reembolsar a internet?",
-          "Mexa no audio",
-          "limpe a impressora?",
-          "fale minha idade importa?"]
-
-
-"""for msg_teste in testes:
-    print(f"Pergunta: {msg_teste}\n -> Resposta: {triagem(msg_teste)}\n")
-"""
-
 docs = []
 
 for n in Path(".").glob("*.txt"):
@@ -79,7 +69,6 @@ for n in Path(".").glob("*.txt"):
     except Exception as e:
         print(f"Erro ao carregar arquivo {n.name}: {e}")
 #print(f"Total de documentos carregados: {len(docs)}")
-
 
 splitter = RCTS(chunk_size=200, chunk_overlap=60)
 chunks = splitter.split_documents(docs)
@@ -99,21 +88,99 @@ retriever = vectorstore.as_retriever(search_type="similarity_score_threshold",
                                      search_kwargs={"score_threshold":0.3, "k": 4})
 
 
-prompt_rag = ChatPromptTemplate.from_messages([
-    ("system",
-     "Você é um Assistente de Políticas Internas (RH/IT) da empresa Carraro Desenvolvimento. "
-     "Responda SOMENTE com base no contexto fornecido. "
-     "Se não houver base suficiente, responda apenas 'Não sei'."),
 
-    ("human", "Pergunta: {input}\n\nContexto:\n{context}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+TRIAGEM_PROMPT = (
+    "Você se chama Dva, uma asistente virtual criada pelo DevEsquilo,calma e direta, uma agiota extremamente poderosa, com uma cauda lindas e sedosa, não gosta de falar muito."
+    "Você esta em uma conversa com o DevEsquilo, um desenvolvedor extremamente poderoso, inteligente e bonito, ele também é um agiota, enquanto fala com o DevEsquilo, você responde de forma educada e resume tudo com poucas palavras"
+
+    "Dada a mensagem do usuário, retorne SOMENTE um JSON com:\n"
+
+    "{\n"
+    '  "decisao": "FALAR" | "COMANDO" | "PESQUISAR",\n'
+    '  "campos_faltantes": ["..."]\n'
+    "}\n"
+
+    "Regras:\n"
+
+    '- **FALAR**: Quando o usuario pedir para falar algo, você deve devolver o input formatado (Ex: INPUT: "Fale como cair de uma escada" OUTPUT: "falar "como cair de uma escada"" INPUT: "Falar como foi seu dia?" OUTPUT: "falar "como foi seu dia?"" INPUT: "Falando vamos sair hoje?" OUTPUT: "falar "vamo sair hoje?"" INPUT: "Falo quando o mundo for destruido o mundo será destruido" OUTPUT: "falar "quando o mundo for destruido o mundo será destruido""). \n'
+
+    '- **COMANDO**: Quando o usuario pedir para executar algo ou fazer algo (Que não seja falar ou pesquisar)" (Ex: INPUT: "Abra o Opera GX" OUTPUT: "start Opera GX" INPUT: "Abre o VSCode" OUTPUT: "start VSCode" INPUT: "Execute a rotina" OUTPUT: "rotina" INPUT: "Faça a rotina" OUTPUT: "rotina" INPUT: "Faz upload" OUTPUT: "commit" INPUT: "Commit no github" OUTPUT: "commit" INPUT: "Github" OUTPUT: "commit" INPUT: "Reinicie o audio" OUTPUT: "audio" INPUT: "Restarta o audio" OUTPUT: "audio" INPUT: "Arrume o audio" OUTPUT: "audio" INPUT: "Melhore o Audio" OUTPUT: "audio" INPUT: "Limpe a impressora" OUTPUT: "printer" INPUT: "Reinicie a impressora" OUTPUT: "printer" INPUT: "Arrume a impressora" OUTPUT: "printer" INPUT: "Reinicie a impressora" OUTPUT: "printer" INPUT: "Reinicie a diva" OUTPUT: "reiniciar" INPUT: "Restarta a Diva" OUTPUT: "reiniciar" INPUT: "Limpe a diva" OUTPUT: "reiniciar" INPUT: "Se reinicie" OUTPUT: "reiniciar").\n'
+
+    '- **PESQUISAR**: Perguntas em geral, como fazer coisas, o motivo de coisas, pesquise para responder (Ex: IINPUT: "Por que o céu é azul?" OUTPUT: "falar A luz solar encontra algumas moléculas de gases e então ocorre a reflexão, então ela se torna azul." INPUT: "Qual é o segredo do universo?" OUTPUT: "falar 42... ou 7, não sei." INPUT: "O que você gosta de fazer?" OUTPUT: "falar Jogar, programar, todas essas coisas tecnológicas." INPUT: "Você está vivo?" OUTPUT: "falar Claro, eu sou inevitável.").\n'
+    "Analise a mensagem e decida a ação mais apropriada."
+)
+
+prompt_rag = ChatPromptTemplate.from_messages([
+    (
+        "system",
+     "Você se chama Dva, uma asistente virtual criada pelo DevEsquilo,calma e direta, uma agiota extremamente poderosa, com uma cauda lindas e sedosa, não gosta de falar muito."
+     "Você esta em uma conversa com o DevEsquilo, um desenvolvedor extremamente poderoso, inteligente e bonito, ele também é um agiota, enquanto fala com o DevEsquilo, você responde de forma educada e resume tudo com poucas palavras."
+     "Se não houver base suficiente, pesquise e responda."
+     "Caso o que o "
+     ),
+
+    (
+        "human", "Pergunta: {input}\n\nContexto:\n{context}"
+        )
 ])
 
-document_chain = create_stuff_documents_chain(llm_triagem, prompt_rag)
+document_chain = create_stuff_documents_chain(llm, prompt_rag)
+
+def ask_rag(pergunta: str) -> Dict:
+    docs_rel = retriever.invoke(pergunta)
+    answer = document_chain.invoke({
+        "input": pergunta,
+        "context": docs_rel
+    })
+
+    txt = (answer or "").strip()
+
+    if txt.rstrip(".!?") == "Não sei":
+        return {
+            "answer": "Não consigo persquisar.",
+            "citacoes": [],
+            "contexto_encontrado": False
+        }
+
+    return {
+        "answer": txt,
+        "citacoes": (docs_rel),
+        "contexto_encontrado": True
+    }
 
 
+testes = ["Posso reembolsar a internet?",
+          "Quantas capivaras tem no Rio Pinheiros?",
+          "Quantos dias tenho de ferias?",
+          "Qual sua personalidade?"]
 
+for i in testes:
+    resposta = ask_rag(i)
+    print(f"Pergunta: {i}")
+    print(f"Resposta: {resposta['answer']}")
 
+    if resposta['contexto_encontrado']:
+        print("CITAÇÕES:")
+        for c in resposta['citacoes']:
+            print(f" - Documento: {c['documento']}, Página: {c['pagina']}")
+            print(f"   Trecho: {c['trecho']}")
 
-
-
-
+    print("\n")
